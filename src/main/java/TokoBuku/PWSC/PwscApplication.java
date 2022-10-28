ca4dea6f7e9fa3e617699a3ab2e7d026cd140bf8
niaaluf <niaaudita01@gmail.com>
2022-10-28 20:39:44 +0700

    index

---
+++ src/main/java/TokoBuku/PWSC/PwscApplication.java
@@ -0,0 +1,13 @@
+package TokoBuku.PWSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TokoBuku/PWSC/PwscApplicationTests.java
@@ -0,0 +1,13 @@
+package TokoBuku.PWSC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
